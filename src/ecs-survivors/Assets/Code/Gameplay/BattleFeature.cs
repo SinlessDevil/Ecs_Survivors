267e40ba09674dac03f302afeb3481697367dc98
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-02-26 16:45:31 +0200

    Added ChaseHeroSystem

--- src/ecs-survivors/Assets/Code/Gameplay/BattleFeature.cs
+++ src/ecs-survivors/Assets/Code/Gameplay/BattleFeature.cs
@@ -1,4 +1,5 @@
 ﻿using Code.Common.Destruct;
+using Code.Gameplay.Features.Enemies;
 using Code.Gameplay.Features.Hero;
 using Code.Gameplay.Features.Movement;
 using Code.Gameplay.Input;
@@ -12,6 +13,7 @@ namespace Code.Gameplay
         {
             Add(systemFactory.Create<InputFeature>());
             Add(systemFactory.Create<HeroFeature>());
+            Add(systemFactory.Create<EnemyFeature>());
             Add(systemFactory.Create<MovementFeature>());
             Add(systemFactory.Create<ProcessDestructedFeature>());
         }
