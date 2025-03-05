49d40fe483b55c20b5a969faf715fba60b75c132
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-03-05 20:23:54 +0200

    Improved Enemy Drop Loot System

--- src/ecs-survivors/Assets/Code/Gameplay/Features/Enemies/Systems/EnemyDropLootSystem.cs
+++ src/ecs-survivors/Assets/Code/Gameplay/Features/Enemies/Systems/EnemyDropLootSystem.cs
@@ -11,14 +11,23 @@ namespace Code.Gameplay.Features.Enemies.Systems
         private readonly IGroup<GameEntity> _enemies;
         private readonly List<GameEntity> _buffer = new(128);
 
+        private readonly List<(LootTypeId, float)> _lootTable = new()
+        {
+            (LootTypeId.HealingItem, 15f),
+            (LootTypeId.PoisonEnchantItem, 15f),
+            (LootTypeId.ExplosionEnchantItem, 15f),
+            (LootTypeId.HexEnchantItem, 15f),
+            (LootTypeId.ExpGem, 40f),
+        };
+
         public EnemyDropLootSystem(GameContext game, ILootFactory lootFactory)
         {
             _lootFactory = lootFactory;
-            
+
             _enemies = game.GetGroup(GameMatcher
                 .AllOf(
-                    GameMatcher.Enemy, 
-                    GameMatcher.Dead, 
+                    GameMatcher.Enemy,
+                    GameMatcher.Dead,
                     GameMatcher.ProcessingDeath,
                     GameMatcher.WorldPosition));
         }
@@ -27,17 +36,28 @@ namespace Code.Gameplay.Features.Enemies.Systems
         {
             foreach (GameEntity enemy in _enemies.GetEntities(_buffer))
             {
-                if (UnityEngine.Random.Range(0, 1f) <= 0.15f)
-                    _lootFactory.CreateLootItem(LootTypeId.HealingItem, enemy.WorldPosition);
-                else if (UnityEngine.Random.Range(0, 1f) <= 0.15f)
-                    _lootFactory.CreateLootItem(LootTypeId.PoisonEnchantItem, enemy.WorldPosition);
-                else if (UnityEngine.Random.Range(0, 1f) <= 0.15f)
-                    _lootFactory.CreateLootItem(LootTypeId.ExplosionEnchantItem, enemy.WorldPosition);
-                else if (UnityEngine.Random.Range(0, 1f) <= 0.15f)
-                    _lootFactory.CreateLootItem(LootTypeId.HexEnchantItem, enemy.WorldPosition);
-                else
-                    _lootFactory.CreateLootItem(LootTypeId.ExpGem, enemy.WorldPosition);
+                LootTypeId selectedLoot = GetRandomLoot();
+                _lootFactory.CreateLootItem(selectedLoot, enemy.WorldPosition);
             }
+        }
+
+        private LootTypeId GetRandomLoot()
+        {
+            float totalWeight = 0f;
+            foreach (var loot in _lootTable)
+                totalWeight += loot.Item2;
+
+            float randomValue = UnityEngine.Random.Range(0, totalWeight);
+            float cumulativeWeight = 0f;
+
+            foreach (var loot in _lootTable)
+            {
+                cumulativeWeight += loot.Item2;
+                if (randomValue < cumulativeWeight)
+                    return loot.Item1;
+            }
+
+            return LootTypeId.ExpGem;
         }
     }
 }
